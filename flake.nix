c3dc54576b7a9ee29d1c3d3e1eb30606c0f424db
fist_it <fist_it@protonmail.com>
2026-04-26 23:54:27 +0200

    tool: Update flake tooling, add .lock file

--- flake.nix
+++ flake.nix
@@ -5,12 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "got";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = null;
+      };
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/got";
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           go
